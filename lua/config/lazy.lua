07c1410155d64795c050b9f355356a7ccccc8008
Haven <137309762+reoden@users.noreply.github.com>
2023-11-01 01:25:40 +0800

    Add files via upload

---
+++ lua/config/lazy.lua
@@ -0,0 +1,28 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  require("plugins.autopairs"),
+  require("plugins.colorscheme"),
+  require("plugins.comment"),
+  require("plugins.neotree"),
+  require("plugins.lualine"),
+  require("plugins.bufferline"),
+  require("plugins.telescope"),
+  require("plugins.lspconfig"),
+  require("plugins.mason"),
+  require("plugins.cmp"),
+  require("plugins.surround"),
+  require("plugins.treesitter"),
+
+})
---
+++ lua/config/options.lua
@@ -0,0 +1,35 @@
+local o = vim.opt
+
+-- line number
+o.number = true
+o.relativenumber = true
+
+-- clipboard
+o.clipboard = "unnamedplus"
+
+--tab
+o.shiftwidth = 2
+o.tabstop = 2
+o.expandtab = true
+
+o.cursorline = true
+
+o.termguicolors = true
+
+o.scrolloff = 10
+
+o.ignorecase = true
+o.smartcase = true
+
+o.signcolumn = "yes"
+
+o.showmode = false
+
+o.mouse = "a"
+
+o.splitbelow = true
+o.splitright = true
+
+o.colorcolumn = "100"
+
+o.swapfile = false
